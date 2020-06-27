ca62e51d50fd0ff9e4a6709b9d679db04736327f
Jorge <jorgeluisyh@gmail.com>
2020-06-27 17:17:12 -0500

    funcion para ocultar campos

--- scripts/main.py
+++ scripts/main.py
@@ -22,6 +22,38 @@ def cortar_region(feature, region):
     clip_region = arcpy.Clip_analysis(feature, fc_region, os.path.join(SCRATCH, "clip_region_{}".format(name_uuid)))
     return clip_region
 
+def hidefields(lyr,*args)
+    """
+    funcion para ocultar campos de una capa
+    lyr = capa a ser procesada
+    args = nombre de los campos excepcion que no se ocultaran
+    output = mfl con todos los campos apagados a excepcion de 'args'
+    """
+    name = os.path.basename(lyr)
+    desc = arcpy.Describe(lyr)
+    field_info = desc.fieldInfo
+
+    fields = [x.name for x in desc.fields]
+
+    # List of fields to hide
+    # desc.OIDFieldName is the name of the 'FID' field
+    fields.remove(desc.OIDFieldName)
+
+    # campos a mantenerse en el lyr
+    if args:
+        for f in args:
+            fields.remove(f)
+
+    # los campos que se ocultaran
+    fieldsToHide = fields
+
+    for i in range(0, field_info.count):
+        if field_info.getFieldName(i) in fieldsToHide:
+            field_info.setVisible(i, "HIDDEN")
+
+    outlyr = arcpy.MakeFeatureLayer_management(lyr, name, "", "", field_info)
+    return outlyr
+
 def red_vial(via_nacional, via_departamental, via_vecinal):
     '''
     Devuelve las capas de lineas y poligonos a partir de la capa de vias con merge corregido
