ce9c6961499a26c656fc8f7853387f18720532cf
Jorge <jorgeluisyh@gmail.com>
2020-09-13 18:50:03 -0500

    agregados p03,p04,p05

--- scripts/main2.py
+++ scripts/main2.py
@@ -68,7 +68,7 @@ def area_evaluar(feature, region):
     field_dissol = ""
     statistics = [[field_dissol, "MAX"]]
     field_area = ""
-    evaluar = seleccionar_region(feature.path, REGIONES[region], os.path.join(SCRATCH, "EVALUAR"))
+    evaluar = seleccionar_region(feature.path, REGIONES[region][0], os.path.join(SCRATCH, "EVALUAR"))
     dissol = dissolve(evaluar, field_dissol, statistics, os.path.join(SCRATCH, "EV_DISSOL"))
     add_field(dissol, field_area)
     calculate_area(dissol, field_area, mod_geom="")
@@ -77,7 +77,7 @@ def area_evaluar(feature, region):
 def p01_anp(region, area_eval, output):
     field_dissol = ""
     statistics = [[field_dissol, "MAX"]]
-    anp = seleccionar_region(gpo_anp().path, REGIONES[region], os.path.join(SCRATCH, "anp_{}".format(region)))
+    anp = seleccionar_region(gpo_anp().path, REGIONES[region][0], os.path.join(SCRATCH, "anp_{}".format(region)))
     anp_intersect = intersect_layers(anp, area_eval, os.path.join(SCRATCH, "INTERSECT"))
     anp_dissol = dissolve(anp_intersect, field_dissol, statistics, output)
     return anp_dissol
@@ -96,6 +96,54 @@ def p02_brturis(region, area_eval, feature_final, output):
     calculate_area(brturis_dissol, field_area, mod_geom="")
     calculate_ptj(brturis_dissol, field_area, "")
     table_join = field_join(brturis_dissol, feature_final, field_id, fields={})
+    return table_join
+
+def p03_cagri(region, area_eval, feature_final, output):
+    field_id = ""
+    field_dissol = ""
+    statistics = [[field_dissol, "MAX"]]
+    field_area = ""
+    field_ptj = ""
+    cagri = seleccionar_region(gpo_cagri().path, REGIONES[region], os.path.join(SCRATCH, "cagri_{}".format(region)))
+    cagri_intersect = intersect_layers(cagri, area_eval, os.path.join(SCRATCH, "INTERSECT"))
+    cagri_dissol = dissolve(cagri_intersect, field_dissol, statistics, output)
+    add_field(cagri_dissol, field_area)
+    add_field(cagri_dissol, field_ptj)
+    calculate_area(cagri_dissol, field_area, mod_geom="")
+    calculate_ptj(cagri_dissol, field_area, "")
+    table_join = field_join(cagri_dissol, feature_final, field_id, fields={})
+    return table_join
+
+def p04_rdef(region, area_eval, feature_final, output):
+    field_id = ""
+    field_dissol = ""
+    statistics = [[field_dissol, "MAX"]]
+    field_area = ""
+    field_ptj = ""
+    rdef = seleccionar_region(gpo_rdef().path, REGIONES[region], os.path.join(SCRATCH, "rdef_{}".format(region)))
+    rdef_intersect = intersect_layers(rdef, area_eval, os.path.join(SCRATCH, "INTERSECT"))
+    rdef_dissol = dissolve(rdef_intersect, field_dissol, statistics, output)
+    add_field(rdef_dissol, field_area)
+    add_field(rdef_dissol, field_ptj)
+    calculate_area(rdef_dissol, field_area, mod_geom="")
+    calculate_ptj(rdef_dissol, field_area, "")
+    table_join = field_join(rdef_dissol, feature_final, field_id, fields={})
+    return table_join
+
+def p05_roam(region, area_eval, feature_final, output):
+    field_id = ""
+    field_dissol = ""
+    statistics = [[field_dissol, "MAX"]]
+    field_area = ""
+    field_ptj = ""
+    roam = seleccionar_region(gpo_roam().path, REGIONES[region], os.path.join(SCRATCH, "roam_{}".format(region)))
+    roam_intersect = intersect_layers(roam, area_eval, os.path.join(SCRATCH, "INTERSECT"))
+    roam_dissol = dissolve(roam_intersect, field_dissol, statistics, output)
+    add_field(roam_dissol, field_area)
+    add_field(roam_dissol, field_ptj)
+    calculate_area(roam_dissol, field_area, mod_geom="")
+    calculate_ptj(roam_dissol, field_area, "")
+    table_join = field_join(roam_dissol, feature_final, field_id, fields={})
     return table_join
 
 
