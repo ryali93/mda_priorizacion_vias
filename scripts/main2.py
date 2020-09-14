f1074603f08c3176d6169a7004b466b2bf3a92f1
Jorge <jorgeluisyh@gmail.com>
2020-09-13 21:40:11 -0500

    p06,07,08 agregados

--- scripts/main2.py
+++ scripts/main2.py
@@ -93,11 +93,12 @@ def p01_anp(region, area_eval, output):
     return anp_dissol
 
 def p02_brturis(region, area_eval, feature_final, output):
-    field_id = ""
-    field_dissol = ""
-    statistics = [[field_dissol, "MAX"]]
-    field_area = ""
-    field_ptj = ""
+    field_id = "ID_EV"
+    field_dissol = ["BRV", "ID_EV"]
+    # statistics = [[field_dissol, "MAX"]]
+    statistics = ""
+    field_area = "A_RTURIS"
+    field_ptj = "P_RTURIS"
     brturis = seleccionar_region(gpo_brturis().path, REGIONES[region], os.path.join(SCRATCH, "brturis_{}".format(region)))
     brturis_intersect = intersect_layers(brturis, area_eval, os.path.join(SCRATCH, "INTERSECT"))
     brturis_dissol = dissolve(brturis_intersect, field_dissol, statistics, output)
@@ -109,11 +110,12 @@ def p02_brturis(region, area_eval, feature_final, output):
     return table_join
 
 def p03_cagri(region, area_eval, feature_final, output):
-    field_id = ""
-    field_dissol = ""
-    statistics = [[field_dissol, "MAX"]]
-    field_area = ""
-    field_ptj = ""
+    field_id = "ID_EV"
+    field_dissol = ["BRV", "ID_EV"]
+    # statistics = [[field_dissol, "MAX"]]
+    statistics = ""
+    field_area = "A_CAGRI"
+    field_ptj = "P_CAGRI"
     cagri = seleccionar_region(gpo_cagri().path, REGIONES[region], os.path.join(SCRATCH, "cagri_{}".format(region)))
     cagri_intersect = intersect_layers(cagri, area_eval, os.path.join(SCRATCH, "INTERSECT"))
     cagri_dissol = dissolve(cagri_intersect, field_dissol, statistics, output)
@@ -125,11 +127,11 @@ def p03_cagri(region, area_eval, feature_final, output):
     return table_join
 
 def p04_rdef(region, area_eval, feature_final, output):
-    field_id = ""
-    field_dissol = ""
-    statistics = [[field_dissol, "MAX"]]
-    field_area = ""
-    field_ptj = ""
+    field_id = "ID_EV"
+    field_dissol = ["BRV", "ID_EV"]
+    statistics = [["PROB", "MEDIAN"]]
+    field_area = "A_RDEF"
+    field_ptj = "P_RDEF"
     rdef = seleccionar_region(gpo_rdef().path, REGIONES[region], os.path.join(SCRATCH, "rdef_{}".format(region)))
     rdef_intersect = intersect_layers(rdef, area_eval, os.path.join(SCRATCH, "INTERSECT"))
     rdef_dissol = dissolve(rdef_intersect, field_dissol, statistics, output)
@@ -141,11 +143,12 @@ def p04_rdef(region, area_eval, feature_final, output):
     return table_join
 
 def p05_roam(region, area_eval, feature_final, output):
-    field_id = ""
-    field_dissol = ""
-    statistics = [[field_dissol, "MAX"]]
-    field_area = ""
-    field_ptj = ""
+    field_id = "ID_EV"
+    field_dissol = ["BRV", "ID_EV"]
+    # statistics = [[field_dissol, "MAX"]]
+    statistics = ""
+    field_area = "A_ROAM"
+    field_ptj = "P_ROAM"
     roam = seleccionar_region(gpo_roam().path, REGIONES[region], os.path.join(SCRATCH, "roam_{}".format(region)))
     roam_intersect = intersect_layers(roam, area_eval, os.path.join(SCRATCH, "INTERSECT"))
     roam_dissol = dissolve(roam_intersect, field_dissol, statistics, output)
@@ -155,6 +158,37 @@ def p05_roam(region, area_eval, feature_final, output):
     calculate_ptj(roam_dissol, field_area, "")
     table_join = field_join(roam_dissol, feature_final, field_id, fields={})
     return table_join
+
+def p06_zdegra(region, area_eval, feature_final, output):
+    field_id = "ID_EV"
+    field_dissol = ["BRV", "ID_EV"]
+    # statistics = [[field_dissol, "MAX"]]
+    statistics = ""
+    field_area = "A_ZDEGRA"
+    field_ptj = "P_ZDEGRA"
+    zdegra = seleccionar_region(gpo_zdegra().path, REGIONES[region], os.path.join(SCRATCH, "zdegra_{}".format(region)))
+    zdegra_intersect = intersect_layers(zdegra, area_eval, os.path.join(SCRATCH, "INTERSECT"))
+    zdegra_dissol = dissolve(zdegra_intersect, field_dissol, statistics, output)
+    add_field(zdegra_dissol, field_area)
+    add_field(zdegra_dissol, field_ptj)
+    calculate_area(zdegra_dissol, field_area, mod_geom="")
+    calculate_ptj(zdegra_dissol, field_area, "")
+    table_join = field_join(zdegra_dissol, feature_final, field_id, fields={})
+    return table_join
+
+def p07_dist(region, area_eval, feature_final, output):
+    field_dissol = ["ID_EV"]
+    statistics = [["PSOCIAL", "MEDIAN"], ["PVBPA", "MEDIAN"]]
+    dist = seleccionar_region(gpo_limitedistrital().path, REGIONES[region], os.path.join(SCRATCH, "dist_{}".format(region)))
+    dist_intersect = intersect_layers(dist, area_eval, os.path.join(SCRATCH, "INTERSECT"))
+    dist_dissol = dissolve(dist_intersect, field_dissol, statistics, output)
+
+def p08_ccpp(region, area_eval, feature_final, output):
+    field_dissol = ["ID_EV"]
+    statistics = [["POBCCPP", "SUM"]]
+    ccpp = seleccionar_region(gpt_ccpp().path, REGIONES[region], os.path.join(SCRATCH, "ccpp_{}".format(region)))
+    ccpp_intersect = intersect_layers(ccpp, area_eval, os.path.join(SCRATCH, "INTERSECT"))
+    ccpp_dissol = dissolve(ccpp_intersect, field_dissol, statistics, output)
 
 
 def process():
